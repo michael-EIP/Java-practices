131271280f34aa59e47b4112fa1d0d64dea5097f
Minh <minhuitk9@gmail.com>
2021-11-17 00:02:17 +0900

    ~ First commit

---
+++ 1. Web service practice/spring-in-5-steps/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearchFor)
+	{
+		//Sorting the array by using Bubble sort algorithm
+		BubbleSortAlgorithm bubbleSort = new BubbleSortAlgorithm();
+		int sortedNums = bubbleSort.sort(numbers);
+		
+		//Search the array
+		
+		
+		//Return the array
+		
+		return 0;
+	}
+	
+	
+	
+	
+}
