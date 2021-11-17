1a67b924664d8a1ff6dfe23558385479759005c3
Minh <minhuitk9@gmail.com>
2021-11-17 17:36:40 +0900

    ~

--- 1. Web service practice/spring-in-5-steps/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
+++ 1. Web service practice/spring-in-5-steps/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,7 +1,12 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class BinarySearchImpl {
 	
+	@Autowired
 	private SortAlgorithm sortAlgorithm;
 	
 	public BinarySearchImpl(SortAlgorithm sortAlgoritm) {
@@ -16,6 +21,7 @@ public class BinarySearchImpl {
 		//Sorting the array by using Bubble sort algorithm
 		int[] sortedNums = sortAlgorithm.sort(numbers);
 		
+		System.out.println(sortAlgorithm);
 		//Search the array
 		
 		
--- 1. Web service practice/spring-in-5-steps/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ 1. Web service practice/spring-in-5-steps/spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,16 +2,25 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+//What are beans?
+//What are dependencies of a bean?
+//Where to search for beans?
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		//BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+		
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		
 		int result = binarySearch.binarySearch(new int[] {125,5}, 3);
 		System.out.println(result);
 		
-		SpringApplication.run(SpringIn5StepsApplication.class, args);
+		
 	}
 
 }
